7012b192da48a509ec1248e9f56cc977d3d53f6e
David Wertenteil <dwertent@cyberarmor.io>
2020-06-18 18:18:40 +0300

    looping

--- ca/signall.sh
+++ ca/signall.sh
@@ -3,7 +3,7 @@ set -x
 NAMESPACE=dev
 DEPLOYMENTS=`kubectl get deployment -n $NAMESPACE | tail -n +2  | awk '{print $1}'`
 
-for DEPLOYMENT in "${DEPLOYMENTS[@]}"; do
+for DEPLOYMENT in $DEPLOYMENTS; do
   ./signone.sh "$DEPLOYMENT" &
   sleep 3
 done
