dab1d8eb63ca663af86bf171e11a0057533ec241
David Wertenteil <dwertent@cyberarmor.io>
2020-06-18 18:12:03 +0300

    slower signing

--- ca/signall.sh
+++ ca/signall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+set -x
 NAMESPACE=dev
 DEPLOYMENTS=`kubectl get deployment -n $NAMESPACE | tail -n +2  | awk '{print $1}'`
 
 for DEPLOYMENT in "${DEPLOYMENTS[@]}"; do
   ./signone.sh "$DEPLOYMENT" &
+  sleep 3
 done
