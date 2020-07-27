e842be817cd0e31039043764c8e042bf536b02e9
David Wertenteil <dwertent@cyberarmor.io>
2020-07-27 14:36:06 +0300

    sp name

--- ca/signone.sh
+++ ca/signone.sh
@@ -5,28 +5,26 @@ CLUSTER=HipsterShopCluster$DEMO_NUMBER
 NAMESPACE=dev
 NAMESPACE_PROD=prod
 DEPLOYMENT="$1"
+SP_NAME=signing-profile-$DEMO_NUMBER-$DEPLOYMENT
 
 wlid="wlid://cluster-$CLUSTER/namespace-$NAMESPACE/deployment-$DEPLOYMENT"
-cacli sp delete -n signing-profile-$DEPLOYMENT &> /dev/null
+cacli sp delete -n $SP_NAME &> /dev/null
 container_name=`cacli wt get -wlid $wlid | python3 -c "import json,sys;print(json.load(sys.stdin)['containers'][0]['name'])"`
 echo "wlid: $wlid, container_name: $container_name"
-cacli sp generate -wlid $wlid -n $container_name -spn signing-profile-$DEPLOYMENT &> log.txt ||  (echo Failed to generate for "$DEPLOYMENT" && cat log.txt)
+cacli sp generate -wlid $wlid -n $container_name -spn $SP_NAME &> log.txt ||  (echo Failed to generate for $DEPLOYMENT && cat log.txt)
 
 # patch_loadgenerator.py
 if [ $DEPLOYMENT == "loadgenerator" ] ;then
   echo Patching loadgenerator
   tmpfile=$(mktemp /tmp/sp.XXXXXX)
-  cacli sp get -n signing-profile-$DEPLOYMENT | python3 patch_loadgenerator.py > "$tmpfile"
-  cacli sp delete -n signing-profile-$DEPLOYMENT
+  cacli sp get -n $SP_NAME | python3 patch_loadgenerator.py > $tmpfile
+  cacli sp delete -n $SP_NAME
   cacli sp create -i "$tmpfile"
   rm "$tmpfile"
 fi
 wlid_prod="wlid://cluster-$CLUSTER/namespace-$NAMESPACE_PROD/deployment-$DEPLOYMENT"
-tmpfile=$(mktemp /tmp/wt.XXXXXX)
-cacli wt get -wlid $wlid | sed 's/dev/prod/g' > "$tmpfile"
-cacli wt apply -i "$tmpfile" && rm "$tmpfile"
-
+cacli wt update -wlid $wlid_prod -sp $SP_NAME
 cacli sign -wlid $wlid_prod -c $container_name
-#cacli sign -wlid $wlid_prod -c $container_name &
+
 #pids[${DEPLOYMENT}]=$!
-echo Signed $DEPLOYMENT
+echo "Signed $DEPLOYMENT, signing profile name: $SP_NAME"
