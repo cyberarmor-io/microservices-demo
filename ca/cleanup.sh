4d2dc59dc5203502870c533d4aaf0a1383cf7fc7
David Wertenteil <dwertent@cyberarmor.io>
2020-08-18 11:41:57 +0300

    sleep after delete ns

--- ca/cleanup.sh
+++ ca/cleanup.sh
@@ -3,6 +3,7 @@ set -x
 cacli login -e ${CA_ENVIRONMENT} -u ${CA_USERNAME} -p ${CA_PASSWORD} -c ${CA_CUSTOMER}
 
 kubectl delete namespace dev prod cyberarmor-system || true
+sleep 20
 #cacli np delete -n HipsterShop-Basic-Policy$DEMO_NUMBER || true
 #cacli np delete -n HipsterShop-Cross-Namespace-Deny-Policy$DEMO_NUMBER || true
 #cacli np delete -n HipsterShop-Block-Policy$DEMO_NUMBER || true
@@ -10,5 +11,4 @@ np_result=`cacli np list  |grep HipsterShop"$DEMO_NUMBER" | sed s/\,//g | xargs 
 echo "$np_result"
 inp_result=`cacli inp list  |grep HipsterShop"$DEMO_NUMBER" | sed s/\,//g | xargs -l1 cacli inp delete -n ` || true
 echo "$inp_result"
-cacli wt list | python3 -c "import json,sys;d=json.load(sys.stdin);print('\n'.join(filter(lambda s: s.count('cluster-HipsterShopCluster$DEMO_NUMBER'),d)))" | xargs -L1 cacli cleanup -wlid "$@" || true
 cacli cluster unregister -n HipsterShop"$DEMO_NUMBER"
