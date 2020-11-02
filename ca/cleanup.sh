a3e58a50eda5facb6666655b9e09c4e3fbf52a63
David Wertenteil <dwertent@cyberarmor.io>
2020-11-02 15:09:07 +0200

    fixed messgae

--- ca/cleanup.sh
+++ ca/cleanup.sh
@@ -6,17 +6,16 @@ kubectl delete namespace dev prod cyberarmor-system || true
 i="0"
 namespaces=("prod" "dev" "cyberarmor-system")
 
-while [ $i -lt 18 ] # wait up to three minutes for all namespace to stop running
-do
-  i=$[$i+1]
-  for (( ns=0; ns<${#namespaces[@]}; ns++ )); do
-    pods=$(kubectl -n ${namespaces[$ns]} get pods 2>&1)
-    if [ "$pods" = "No resources found." ]; then
+while [ $i -lt 18 ]; do # wait up to three minutes for all namespace to stop running
+  i=$(($i + 1))
+  for ((ns = 0; ns < ${#namespaces[@]}; ns++)); do
+    pods=$(kubectl -n "${namespaces[$ns]}" get pods 2>&1)
+    if [ "$pods" = "No resources found in ${namespaces[$ns]} namespace." ] || [ "$pods" = "No resources found." ]; then # "No resources found."
       echo "namespaces ${namespaces[$ns]} not running"
-      namespaces=( "${namespaces[@]:0:$ns}" "${namespaces[@]:$((ns + 1))}" ) # remove namespace from list
+      namespaces=("${namespaces[@]:0:$ns}" "${namespaces[@]:$((ns + 1))}") # remove namespace from list
     fi
     if [ ${#namespaces[@]} -eq 0 ]; then
-      i=19
+      i="19"
       break
     fi
   done
@@ -25,8 +24,8 @@ done
 
 echo "all namespace are not running, unregistering cluster"
 
-np_result=`cacli np list  |grep HipsterShopCluster"$DEMO_NUMBER" | sed s/\,//g | xargs -l1 cacli np delete -n ` || true
+np_result=$(cacli np list | grep HipsterShopCluster"$DEMO_NUMBER" | sed s/\,//g | xargs -l1 cacli np delete -n) || true
 echo "$np_result"
-inp_result=`cacli inp list  |grep HipsterShopCluster"$DEMO_NUMBER" | sed s/\,//g | xargs -l1 cacli inp delete -n ` || true
+inp_result=$(cacli inp list | grep HipsterShopCluster"$DEMO_NUMBER" | sed s/\,//g | xargs -l1 cacli inp delete -n) || true
 echo "$inp_result"
 cacli cluster unregister -n HipsterShopCluster"$DEMO_NUMBER"
