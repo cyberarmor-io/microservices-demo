ea19fdffbeddad00dd071e2985a3f308ef0ae30f
Ben Hirschberg <bhirschb@cyberarmor.io>
2021-01-27 09:23:19 +0000

    secret dix

--- demo/protect-hipster.sh
+++ demo/protect-hipster.sh
@@ -10,7 +10,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CLUSTER=`kubectl config current-context`
 cacli cluster register --run -n $CLUSTER 
 sleep 120
-cacli secret encrypt -sid sid://cluster-$CLUSTER/namespace-hipster/secret-top-secret
+cacli secp create -sid sid://cluster-$CLUSTER/namespace-hipster/secret-top-secret
 cacli k8s attach --namespace hipster --cluster $CLUSTER 
 sleep 300
 cacli sign --namespace hipster --cluster $CLUSTER 
