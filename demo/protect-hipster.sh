fb127d5b22df99c101c9eb9a9d40f7393be1ea01
Ben Hirschberg <bhirschb@cyberarmor.io>
2021-02-14 08:38:03 +0200

    shauli requirements 1.0

--- demo/protect-hipster.sh
+++ demo/protect-hipster.sh
@@ -10,7 +10,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CLUSTER=`kubectl config current-context | sed "s/[\.,@]/-/g"`
 cacli cluster register --run -n $CLUSTER 
 sleep 120
-cacli secp create -sid sid://cluster-$CLUSTER/namespace-hipster/secret-top-secret
+#cacli secp create -sid sid://cluster-$CLUSTER/namespace-hipster/secret-ssl-private-key
 cacli k8s attach --namespace hipster --cluster $CLUSTER 
 sleep 300
 cacli sign --namespace hipster --cluster $CLUSTER 
