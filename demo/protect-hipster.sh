e5b85077efd1677309f4d80ab89b8a608655c23f
Ben Hirschberg <bhirschb@cyberarmor.io>
2021-02-15 15:39:59 +0200

    added guestbook + tcp liveliness

--- demo/protect-hipster.sh
+++ demo/protect-hipster.sh
@@ -10,8 +10,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CLUSTER=`kubectl config current-context | sed "s/[\.,@]/-/g"`
 cacli cluster register --run -n $CLUSTER 
 sleep 120
-#cacli secp create -sid sid://cluster-$CLUSTER/namespace-hipster/secret-ssl-private-key
+cacli secp create -sid sid://cluster-$CLUSTER/namespace-hipster/secret-ssl-private-key
 cacli k8s attach --namespace hipster --cluster $CLUSTER 
 sleep 300
+cacli np create --name $CLUSTER-basic-policy --policy_type basic --server_attributes cluster=$CLUSTER --client_attributes cluster=$CLUSTER --server_attributes namespace=hipster --client_attributes namespace=hipster
 cacli sign --namespace hipster --cluster $CLUSTER 
 
