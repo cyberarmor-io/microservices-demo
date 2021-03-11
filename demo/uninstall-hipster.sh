28548832e7d640335b25694c85a74e449ebd56af
david <dwertent@cyberarmor.io>
2021-03-11 17:19:01 +0200

    update frontend

--- demo/uninstall-hipster.sh
+++ demo/uninstall-hipster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-CLUSTER=`kubectl -n cyberarmor-system exec -it $(kubectl get pod -n cyberarmor-system | grep ca-webhook |  awk '{print $1}') -- env | grep CA_CLUSTER_NAME | tr "=" " " | awk '{print $2}'`
+#CLUSTER=`kubectl -n cyberarmor-system exec -it $(kubectl get pod -n cyberarmor-system | grep ca-webhook |  awk '{print $1}') -- env | grep CA_CLUSTER_NAME | tr "=" " " | awk '{print $2}'`
+CLUSTER=`kubectl config current-context | sed "s/[\:\.\/,@,_]/-/g"`
 kubectl delete ns hipster cyberarmor-system
 sleep 120
 cacli cluster unregister -n $CLUSTER 
